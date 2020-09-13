9ad130ad85f5087fb1c45c204b366e3dfd86d068
Coder-T <ngmtungsora@gmail.com>
2020-09-13 12:55:08 +0700

    import component EditUserForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import UserTable from './tables/UserTable';
 import AddUserForm from './forms/AddUserForm';
+import EditUserForm from './forms/EditUserForm';
 
 function App() {
 	return (
 		<div className="App">
 			<AddUserForm />
+			<EditUserForm />
 			<UserTable />
 		</div>
 	);
