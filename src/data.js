709f4bf47b570542b5ae9e66559c8af22c95509f
Coder-T <ngmtungsora@gmail.com>
2020-09-13 12:42:41 +0700

    create database

---
+++ src/data.js
@@ -0,0 +1,52 @@
+const userList = [
+	{
+		id: '1',
+		name: 'Mrs. Robb Wisozk',
+		address: 'it',
+		website: 'https://veda.name',
+		total: 20724,
+		date: '22/08/2020'
+	},
+	{
+		id: '2',
+		name: 'Patsy Ruecker IV',
+		address: 'en_CA',
+		website: 'http://paris.biz',
+		total: 11428,
+		date: '22/08/2020'
+	},
+	{
+		id: '3',
+		name: 'Sallie Klein',
+		address: 'en_GB',
+		website: 'https://georgette.biz',
+		total: 97239,
+		date: '22/08/2020'
+	},
+	{
+		id: '4',
+		name: 'Aleen Jones',
+		address: 'it',
+		website: 'https://novella.com',
+		total: 50565,
+		date: '22/08/2020'
+	},
+	{
+		id: '5',
+		name: 'Dr. Triston Mraz',
+		address: 'en_CA',
+		website: 'https://hillard.info',
+		total: 85720,
+		date: '22/08/2020'
+	},
+	{
+		id: '6',
+		name: 'Erica Pacocha',
+		address: 'es_MX',
+		website: 'https://vernice.name',
+		total: 4458,
+		date: '22/08/2020'
+	}
+];
+
+export default userList;
