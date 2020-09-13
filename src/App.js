1bb0767bbacb13a111dbbeebe7bf83af8bf4ac76
Coder-T <ngmtungsora@gmail.com>
2020-09-13 12:52:10 +0700

    import addUserForm component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import UserTable from './tables/UserTable';
+import AddUserForm from './forms/AddUserForm';
 
 function App() {
 	return (
 		<div className="App">
+			<AddUserForm />
 			<UserTable />
 		</div>
 	);
