b3ce2616dc29087c28541030ed2ad69bdc5ffe33
Coder-T <ngmtungsora@gmail.com>
2020-09-13 12:43:21 +0700

    format table data

---
+++ src/tables/UserTable.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Button, Popconfirm } from 'antd';
+
+const UserTable = () => {
+	const columns = [
+		{
+			title: 'No.',
+			dataIndex: 'id',
+			key: 'key',
+			render: key => <div>{key}</div>
+		},
+		{
+			title: 'Name',
+			dataIndex: 'name',
+			key: 'name',
+			render: text => <a>{text}</a>
+		},
+		{
+			title: 'Address',
+			dataIndex: 'address',
+			key: 'address'
+		},
+		{
+			title: 'Website',
+			dataIndex: 'website',
+			key: 'website'
+		},
+		{
+			title: 'Total',
+			dataIndex: 'total',
+			key: 'total',
+			render: total => <div>{total}</div>
+		},
+		{
+			title: 'Date',
+			key: 'date',
+			dataIndex: 'date'
+			// render: date => <div>{date}</div>
+		},
+		{
+			title: 'Act',
+			width: '14%',
+			align: 'center',
+			render: (text, record) =>
+				props.users.length > 0 ? (
+					<div>
+						<Button
+							style={{
+								background: '#ffc107',
+								borderColor: '#ffc107'
+							}}
+							onClick={() => props.editUser(record.id, record)}
+						>
+							<i className="fa fa-edit">Edit</i>
+						</Button>
+						<Popconfirm
+							title="Definitely Delete"
+							onConfirm={() => props.deleteUser(record.id)}
+						>
+							<Button
+								style={{
+									background: '#f86c6b',
+									borderColor: '#f86c6b'
+								}}
+							>
+								<i className="fa fa-trash">Delete</i>
+							</Button>
+						</Popconfirm>
+					</div>
+				) : null
+		}
+	];
+
+	return (
+		<Table
+			columns={columns}
+			dataSource={props.users}
+			rowKey={record => record.name}
+		/>
+	);
+};
+
+export default UserTable;
