fbde315fa263d3b4bd76b6230c76fa0a0b73997f
Coder-T <ngmtungsora@gmail.com>
2020-09-13 13:52:05 +0700

    fix {setEditing}

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
 		<div className="App">
 			{editing ? (
 				<EditUserForm
-					editing={editing}
+					setEditing={setEditing}
 					updateUser={updateUser}
 					userCurrent={userCurrent}
 				/>
