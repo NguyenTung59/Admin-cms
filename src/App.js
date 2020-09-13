23d6ed5cb8e1cbfb64df2a830b8e171ecf73c8be
Coder-T <ngmtungsora@gmail.com>
2020-09-13 13:19:51 +0700

    get data users

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+import UserData from './data';
 import UserTable from './tables/UserTable';
 import AddUserForm from './forms/AddUserForm';
 import EditUserForm from './forms/EditUserForm';
 
 function App() {
 	const [editting, setEditting] = useState(false);
+	const [users, setUsers] = useState([]);
 
 	//Get Data
+	const getDate = () => {
+		setUsers(UserData);
+	};
 	//Add user
 	//Delete user
 	//Edit user
@@ -16,7 +23,8 @@ function App() {
 		<div className="App">
 			{editting ? <EditUserForm /> : <AddUserForm />}
 
-			<UserTable />
+			<UserTable users={users} />
+			<Button onClick={getDate}>Get Data</Button>
 		</div>
 	);
 }
